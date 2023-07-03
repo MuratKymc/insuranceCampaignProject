4ae6a96c237d29850f6f981cef8ac88da8d9b766
Murat <murat.kymc.16@gmail.com>
2023-07-03 12:14:54 +0300

    Initial commit

---
+++ src/main/java/com/muratk/insurancecampaignproject/InsuranceCampaignProjectApplication.java
@@ -0,0 +1,13 @@
+package com.muratk.insurancecampaignproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceCampaignProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceCampaignProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muratk/insurancecampaignproject/InsuranceCampaignProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muratk.insurancecampaignproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceCampaignProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
